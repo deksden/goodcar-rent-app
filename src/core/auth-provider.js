4705ab722f8721ec1a86353a5ced78602edceb5b
deksden <deksden@deksden.com>
2020-10-05 22:17:07 +0700

    feat(auth-provider): check /me endpoint to test for expired jwt token

--- src/core/auth-provider.js
+++ src/core/auth-provider.js
@@ -26,64 +26,28 @@ const authProvider = {
     return Promise.resolve()
   },
   checkError: () => Promise.resolve(),
-  checkAuth: () =>
-    window.localStorage.getItem('token') ? Promise.resolve() : Promise.reject(Error('no auth')),
+  checkAuth: () => {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      const request = new Request(`${ApiUrl}/me`, {
+        method: 'GET',
+        headers: new Headers({
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        })
+      })
+      return fetch(request)
+        .then((res) => {
+          if (res.status < 200 || res.status >= 300) {
+            return Promise.reject(Error(`err ${res.status}`))
+          }
+          return Promise.resolve()
+        })
+    } else {
+      return Promise.reject(Error('no auth'))
+    }
+  },
   getPermissions: () => Promise.reject(Error('Unknown method'))
 }
 
 export default authProvider
-
-/*
-export default (type, params) => {
-  if (type === AUTH_LOGIN) {
-    const { username, password } = params
-    const options = {
-      method: 'POST',
-      body: JSON.stringify({ email: username, password }),
-      headers: { 'Content-Type': 'application/json' }
-    }
-
-    return fetch(`${ApiUrl}/auth/login`, options)
-      .then(response => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText)
-        }
-        return response.json()
-      })
-      .then(({ token }) => {
-        window.localStorage.setItem('token', token)
-        console.log(`auth ok, token ${token}`)
-      })
-  }
-  // called when the user clicks on the logout button
-  if (type === AUTH_LOGOUT) {
-    // return fetch(`${ApiUrl}/auth/logout`, options)
-    //   .then(response => {
-    //     if (response.status < 200 || response.status >= 300) {
-    //       throw new Error(response.statusText)
-    //     }
-    window.localStorage.removeItem('token')
-    return Promise.resolve()
-    // })
-  }
-  // called when the API returns an error
-  if (type === AUTH_ERROR) {
-    const { status } = params
-    if (status === 401 || status === 403) {
-      window.localStorage.removeItem('token')
-      return Promise.reject(new Error('Auth error'))
-    }
-    return Promise.resolve()
-  }
-  // called when the user navigates to a new location
-  if (type === AUTH_CHECK) {
-    return window.localStorage.getItem('token')
-      ? Promise.resolve()
-      : Promise.reject(new Error('No auth'))
-  }
-  if (type === AUTH_GET_PERMISSIONS) {
-    return Promise.resolve()
-  }
-  return Promise.reject(new Error('Unknown method'))
-}
-*/
